016e06f3e4ffb3b3c44052f60037aac738b04150
Tia <lyz15129392021@gmail.com>
2021-11-11 22:49:38 +0800

    Initial commit

---
+++ app/src/main/java/com/LiYueZhe2019054505/myaccount/activity_startup.java
@@ -0,0 +1,14 @@
+package com.LiYueZhe2019054505.myaccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_startup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_startup);
+    }
+}
